7a70c0436619c8d8bc0102ee5c39b9dc6e11509c
Andrei Klimkou <AAkilmkov@gmail.com>
2020-12-12 13:23:56 +0300

    feat: commit console.log

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 
-function angleBetweenClockHands(date) {
-  const h = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
-  const min = date.getMinutes();
-  const angle = Math.abs(0.5 * (60 * h - 11 * min)) > 180
-    ? (360 - Math.abs(0.5 * (60 * h - 11 * min))) / 180 : Math.abs(0.5 * (60 * h - 11 * min)) / 180;
-  console.log(h);
-  console.log(min);
-  console.log(angle);
-  console.log(Math.PI * angle);
-  return Math.PI * angle;
-}
-const time = new Date('December 17, 1995 18:00:00');
-angleBetweenClockHands(time);
+// function angleBetweenClockHands(date) {
+//   const h = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+//   const min = date.getMinutes();
+//   const angle = Math.abs(0.5 * (60 * h - 11 * min)) > 180
+//     ? (360 - Math.abs(0.5 * (60 * h - 11 * min))) / 180 :
+//  Math.abs(0.5 * (60 * h - 11 * min)) / 180;
+//   console.log(h);
+//   console.log(min);
+//   console.log(angle);
+//   console.log(Math.PI * angle);
+//   return Math.PI * angle;
+// }
+// const time = new Date('December 17, 1995 18:00:00');
+// angleBetweenClockHands(time);
